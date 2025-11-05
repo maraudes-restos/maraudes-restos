e93600a19927bd299edd5fcfb16bb294b3eaf8d4
Antoine Verin <24255551+antoineverin@users.noreply.github.com>
2025-11-05 17:57:05 +0100

    add default car to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,34 @@
 import prisma from "@/lib/prisma";
 
-
+prisma.car.create({
+  data: {
+    name: "Kangoo",
+    licensePlate: "FR-123-WW",
+    status: {
+      createMany: {
+        data: [
+          {
+            type: "BOOKED",
+            comment: "Goes brrrrr",
+            effectiveAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7 days
+          },
+          {
+            type: "OUT_OF_ORDER",
+            comment: "Tire goes brrr",
+          }
+        ]
+      }
+    },
+    trips: {
+      createMany: {
+        data: [
+          {
+            driverFirstname: "Jean",
+            driverLastname: "Eude",
+            driverPhone: "06 06 06 06 05",
+          }
+        ]
+      }
+    }
+  }
+})
