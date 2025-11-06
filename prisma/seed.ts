a36f80913dd850833789490b6ee9dff26e494d58
Antoine Verin <24255551+antoineverin@users.noreply.github.com>
2025-11-06 13:49:29 +0100

    fix seeding

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,34 +1,40 @@
-import prisma from "@/lib/prisma";
+import { PrismaClient } from "../src/generated/prisma";
 
-prisma.car.create({
-  data: {
-    name: "Kangoo",
-    licensePlate: "FR-123-WW",
-    status: {
-      createMany: {
-        data: [
-          {
-            type: "BOOKED",
-            comment: "Goes brrrrr",
-            effectiveAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7 days
-          },
-          {
-            type: "OUT_OF_ORDER",
-            comment: "Tire goes brrr",
-          }
-        ]
-      }
-    },
-    trips: {
-      createMany: {
-        data: [
-          {
-            driverFirstname: "Jean",
-            driverLastname: "Eude",
-            driverPhone: "06 06 06 06 05",
-          }
-        ]
+const prisma = new PrismaClient();
+
+export default async function main() {
+  await prisma.car.create({
+    data: {
+      name: "Kangoo",
+      licensePlate: "FR-123-WW",
+      status: {
+        createMany: {
+          data: [
+            {
+              type: "BOOKED",
+              comment: "Goes brrrrr",
+              effectiveAt: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000) // 7 days
+            },
+            {
+              type: "OUT_OF_ORDER",
+              comment: "Tire goes brrr",
+            }
+          ]
+        }
+      },
+      trips: {
+        createMany: {
+          data: [
+            {
+              driverFirstname: "Jean",
+              driverLastname: "Eude",
+              driverPhone: "06 06 06 06 05",
+            }
+          ]
+        }
       }
     }
-  }
-})
+  })
+}
+
+main();
